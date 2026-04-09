036690d233c66e1ffa9f519da22c710bab7e470d
Javohir <xolmatovjavohir911@gmail.com>
2026-04-09 09:56:02 +0500

    chore: remove start.sh as using render command directly

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Python paketlarini o'rnatish va Admin botni orqa fonda (background) ishga tushirish
-pip install -r admin_bot/requirements.txt
-python3 admin_bot/main.py &
-
-# Asosiy Node.js backend serverini ishga tushirish
-npm start
