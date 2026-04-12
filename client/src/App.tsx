7d1efb18dbf331c7fa9beaad424ca7e0c3df94f7
Javohir <xolmatovjavohir911@gmail.com>
2026-04-12 14:12:56 +0500

    chore: set final Google Analytics ID to G-TSXDSPDL98 (original Yozgo property)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, useLocation } from "wouter";
 import ReactGA from "react-ga4";
-ReactGA.initialize("G-56W2C1S1FV");
+ReactGA.initialize("G-TSXDSPDL98");
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
